7d44129ff84cb5e06830f290ffe95a826d51e04a
robertokbs <robertokbs@gmail.com>
2018-07-11 14:23:49 -0300

    geracao do pdf na mesma pagina, timeout da session para 30min

--- src/main/java/br/com/os/bean/ProdutoOSBean.java
+++ src/main/java/br/com/os/bean/ProdutoOSBean.java
@@ -1,18 +1,30 @@
 package br.com.os.bean;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.sql.Connection;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
@@ -20,16 +32,14 @@ import org.omnifaces.util.Messages;
 import br.com.os.dao.ProdutoOsDAO;
 
 import br.com.os.domain.ProdutoOS;
+import br.com.os.util.GeradorRelatorio;
 import br.com.os.util.HibernateUtil;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporter;
-import net.sf.jasperreports.engine.JRExporterParameter;
+
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperPrintManager;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-
+import net.sf.jasperreports.engine.JasperRunManager;
 
 
 @ManagedBean
@@ -37,7 +47,7 @@ import net.sf.jasperreports.engine.export.JRPdfExporter;
 public class ProdutoOSBean  implements Serializable {
 
 	/**
-	 * 
+	 *  ver http://javasemcafe.blogspot.com/2011/05/jasperreports-401-javabean-datasource.html
 	 */
 	private static final long serialVersionUID = 1L;
 	private ProdutoOS produtoOS;
@@ -80,6 +90,71 @@ public class ProdutoOSBean  implements Serializable {
 		}
 	}
 	
+	
+	 public void gerarRelatorio() {
+	        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+	        ServletContext context = (ServletContext) externalContext.getContext();
+	        String arquivo = context.getRealPath("/reports/itens_os.jasper");
+	        System.out.println(arquivo);
+	 
+	      
+	 
+	        gerarRelatorioWeb(arquivo);
+	    }
+	 
+	    private void gerarRelatorioWeb( String arquivo) {
+	        ServletOutputStream servletOutputStream = null;
+	        FacesContext context = FacesContext.getCurrentInstance();
+	        HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+	 
+	        try {
+	            servletOutputStream = response.getOutputStream();
+	            Connection conexao = HibernateUtil.getConexao();
+	            Map<String, Object> parametros = new HashMap<String, Object>();
+	            
+	           // JasperRunManager.runReportToPdfStream(new FileInputStream(new File(arquivo)), response.getOutputStream(), parametros, conexao);
+	            JasperRunManager.runReportToPdfStream(new FileInputStream(new File(arquivo)), response.getOutputStream(), parametros, conexao);
+	            
+	            response.setContentType("application/pdf");
+	            servletOutputStream.flush();
+	            servletOutputStream.close();
+	            context.renderResponse();
+	            context.responseComplete();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	
+	
+	public void print() throws IOException {
+		try {
+			String jrxml = Faces.getRealPath("/reports/itens_os.jrxml");
+			String jasper = JasperCompileManager.compileReportToFile(jrxml);
+			
+			System.out.println("jasper gerado: " + jasper);
+			
+			Map<String, Object> parametros = new HashMap<String, Object>();
+			Connection conexao = HibernateUtil.getConexao();
+			
+			FacesContext context = FacesContext.getCurrentInstance();
+			HttpServletResponse response  =  (HttpServletResponse) context.getExternalContext().getResponse(); // obtém o response do seu framework mvc
+			OutputStream saida = response.getOutputStream();
+			
+			GeradorRelatorio gerador = new GeradorRelatorio(conexao);
+			gerador.geraPdf("/pages/", parametros, saida);
+			
+			
+		} catch (JRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	public void imprimir() {
 		try {
 			//String caminho = Faces.getRealPath("/reports/itens_os.jasper");
@@ -103,10 +178,7 @@ public class ProdutoOSBean  implements Serializable {
 			
 			//JasperPrintManager.printReport(relatorio, true); //true manda para o control+p do SO. podendo salvar
 			
-			JRExporter exporter = new JRPdfExporter();
-			exporter.setParameter(JRExporterParameter.JASPER_PRINT, relatorio);
-			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, saida);
-			exporter.exportReport();
+			
 			
 		}catch (JRException ex) {
 			Messages.addGlobalError("Erro tentar gerar impressão dos itens de OS");
---
+++ src/main/java/br/com/os/util/GeradorRelatorio.java
@@ -0,0 +1,47 @@
+package br.com.os.util;
+
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+
+public class GeradorRelatorio {
+	
+	private Connection conexao;
+
+    public GeradorRelatorio(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    public void geraPdf(String jrxml, 
+        Map<String, Object> parametros, OutputStream saida) {
+
+        try {
+
+            // compila jrxml em memoria
+            JasperReport jasper = JasperCompileManager.compileReport(jrxml);
+
+            // preenche relatorio
+            JasperPrint print = JasperFillManager.fillReport(jasper, parametros, this.conexao);
+
+            // exporta para pdf
+            JRExporter exporter = new JRPdfExporter();
+            exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, saida);
+
+            exporter.exportReport();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao gerar relatório", e);
+        }
+    }   
+	
+
+}
